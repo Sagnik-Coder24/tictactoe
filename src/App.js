57ee9e924bfe2b146b5e25c8fa30e8a61f57ed15
sag_lyf <gosagnik@gmail.com>
2021-08-22 16:33:58 +0530

    created board and square components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
+import Board from './components/Board';
 
 const App = () => {
-  return (
-    <>
-      <h1>Welcome to React Parcel Micro App!</h1>
-      <p>Hard to get more minimal.</p>
-      <small>Hello sagnik</small>
-    </>
-  );
+    return (
+        <div>
+            <h1>TIC TAC TOE</h1>
+            <Board />
+        </div>
+    );
 };
 
 export default App;
